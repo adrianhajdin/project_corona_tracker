58e6ff2865a14eda91a4615b0a317ff53ef6c43b
Harry Zachariou <harry.zachariou@reachplc.com>
2020-06-03 17:13:36 +0100

    refactored repeating cards into reusable card component

---
+++ src/components/Cards/Card/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
+import cx from 'classnames';
+
+import styles from './Card.module.css';
+
+const CardComponent = ({ className, cardTitle, value, lastUpdate, cardSubtitle }) => (
+  <Grid item xs={12} md={3} component={Card} className={cx(styles.card, className)}>
+    <CardContent>
+      <Typography color="textSecondary" gutterBottom>
+        {cardTitle}
+      </Typography>
+      <Typography variant="h5" component="h2">
+        <CountUp start={0} end={value} duration={2.75} separator="," />
+      </Typography>
+      <Typography color="textSecondary">
+        {new Date(lastUpdate).toDateString()}
+      </Typography>
+      <Typography variant="body2" component="p">
+        {cardSubtitle}
+      </Typography>
+    </CardContent>
+  </Grid>
+);
+
+export default CardComponent;
